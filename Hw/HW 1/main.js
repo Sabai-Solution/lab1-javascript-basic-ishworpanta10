e3d43035f4d2c0d8992fe0b8d1955274fcc6a042
ishworpanta10 <ishworpanta10@gmail.com>
2019-09-22 23:35:28 +0545

    Regex Validation included

--- Hw/HW 1/main.js
+++ Hw/HW 1/main.js
@@ -3,6 +3,7 @@ function showTime(){
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
+    var mili = date.getMilliseconds(); // 0 - 1000
     var session = "AM";
     
     if(h == 0){
@@ -17,12 +18,12 @@ function showTime(){
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
+    mili = (mili < 10) ? "00" + mili : mili;   
+    var time = h + ":" + m + ":" + s + ":"+mili+ " " + session;
     document.getElementById("DigitalClock").innerText = time;
     document.getElementById("DigitalClock").textContent = time;
     
-    setTimeout(showTime, 1000);
+    setTimeout(showTime, 10);
     
 }
 
